9fe9b60aaf862e079d250464d65552fc4a1e7177
Nabin Yadav <nabin.01least@gmail.com>
2023-05-21 11:11:48 +0545

    message1

--- resources/views/website/messagechairman.blade.php
+++ resources/views/website/messagechairman.blade.php
@@ -1,6 +1,6 @@
 @extends("layouts.master")
 @section('content')
-<div class="container-fluid bg-primary py-5 bg-header" style="margin-bottom: 0px;">
+<div class="container-fluid bg-primary py-5 bg-header" style="margin-bottom: 90px;">
     <div class="row py-5">
         <div class="col-12 pt-lg-5 mt-lg-5 text-center">
             
