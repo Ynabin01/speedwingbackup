d9d2c4a1e6f98e08484e20fcc028e6d30a389eac
Nabin Yadav <nabin.01least@gmail.com>
2023-05-21 11:10:46 +0545

    message

--- resources/views/website/messagechairman.blade.php
+++ resources/views/website/messagechairman.blade.php
@@ -1,12 +1,15 @@
 @extends("layouts.master")
 @section('content')
-<div class="container-fluid bg-primary py-5 bg-header" style="margin-bottom: 90px;">
+<div class="container-fluid bg-primary py-5 bg-header" style="margin-bottom: 0px;">
     <div class="row py-5">
         <div class="col-12 pt-lg-5 mt-lg-5 text-center">
-            <h1 class="display-4 text-white animated zoomIn"> Chairman Message</h1>
-            <a href="index.html" class="h5 text-white">Home</a>
+            
+            <h1 class="display-4 text-white animated zoomIn">{{$slug2->caption ?? ''}}</h1>
+            <a href="/" class="h5 text-white">Home</a>
             <i class="far fa-circle text-white px-2"></i>
-            <a href="" class="h5 text-white">Chairman Message</a>
+            @if(isset($slug2))
+            <a href="" class="h5 text-white">{{$slug2->caption ?? ''}}</a>
+            @endif
         </div>
     </div>
 </div>
