3060eb8596c97eb387fa727e9d76e25961017866
Nabin Yadav <nabin.01least@gmail.com>
2023-05-21 11:28:17 +0545

    normal

--- resources/views/website/normal.blade.php
+++ resources/views/website/normal.blade.php
@@ -2,7 +2,20 @@
 
 @section('content')
 
-@include('website.breadcrumb')
+<div class="container-fluid bg-primary py-5 bg-header" style="margin-bottom: 0px;">
+    <div class="row py-5">
+        <div class="col-12 pt-lg-5 mt-lg-5 text-center">
+            
+            <h1 class="display-4 text-white animated zoomIn">{{$slug2->caption ?? ''}}</h1>
+            <a href="/" class="h5 text-white">Home</a>
+            <i class="far fa-circle text-white px-2"></i>
+            @if(isset($slug2))
+            <a href="" class="h5 text-white">{{$slug2->caption ?? ''}}</a>
+            @endif
+        </div>
+    </div>
+</div>
+
  <!-- Full Screen Search Start -->
  <div class="modal fade" id="searchModal" tabindex="-1">
     <div class="modal-dialog modal-fullscreen">
